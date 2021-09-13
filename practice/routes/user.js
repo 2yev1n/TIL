8583fd048bedf0439658bd14ba55e2c25e039187
yebin <yebin5127@gmail.com>
2021-09-13 21:38:28 +0900

    add routes

---
+++ practice/routes/user.js
@@ -0,0 +1,7 @@
+const router = require("express") ();
+const controller = require("../controller/user");
+
+router.post("/sign_up", controller.sign_up);
+router.post("/login", controller.login);
+
+module.exports = router;
